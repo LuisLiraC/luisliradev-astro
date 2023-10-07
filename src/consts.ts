0d4cc98e30868fc4091a8e90ca2dfd3807fa270a
LuisLiraC <luis_ssbm@hotmail.com>
2023-10-07 16:20:07 -0600

    update social links

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Luis Lira';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Software Engineer: QA, Frontend, Backend and Game Development enthusiast';
