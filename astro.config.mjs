eb3eab92091cdcfbe830e836bdefb1e93ca647a2
LuisLiraC <luis_ssbm@hotmail.com>
2023-10-07 15:03:08 -0600

    using custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://luislirac.github.io',
-	base: '/luisliradev-astro',
+	site: 'https://luislira.dev',
 	integrations: [mdx(), sitemap()],
 });
