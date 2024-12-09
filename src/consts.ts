869a31f71f4c4085bbcca9af245df422e35ec2ff
LuisLiraC <luis_ssbm@hotmail.com>
2024-12-08 18:00:31 -0600

    update description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Luis Lira';
-export const SITE_DESCRIPTION = 'Software Engineer: QA, Frontend, Backend and Game Development enthusiast';
+export const SITE_DESCRIPTION = 'Platform Engineer & SDET focused on building tools and systems that enhance developer productivity and workflow automation.';
